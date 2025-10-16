10278b2442eb2ae98ea2aa906de74fb8e2265d5a
ptatoo <i.alexander.song@gmail.com>
2025-10-16 14:53:01 -0700

    Project 2 freeze

---
+++ Project2/hw.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int codeSection = 321;
+
+    switch (codeSection) {
+        case (521):
+            cout << "extortion";
+            break;
+        case (321):
+            cout << "selling illegal lottery tickets";
+            break;
+        case (322):
+            cout << "selling illegal lottery tickets";
+            break;
+        case (383):
+            cout << "selling rancid butter";
+            break;
+        case (598):
+            cout << "injuring a bird in a public cemetery";
+            break;
+        default:
+            cout << "some other crime";
+}
+}
