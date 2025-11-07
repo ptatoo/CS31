ae1ddf31c14b3785e628d437585d9587c636f727
ptatoo <i.alexander.song@gmail.com>
2025-11-07 15:22:59 -0800

    Project 4 Code Done

--- Project3/dance.cpp
+++ Project3/dance.cpp
@@ -5,88 +5,112 @@
 
 using namespace std;
 
-bool isSyntacticallyCorrect(string dance) {
-    string directions = "LlDdUuRr";
+bool isSyntacticallyCorrect(string dance);
 
-    //loops through every beat
-    while (dance.find("/") != string::npos) {
-        //finds contents in beat
+bool isSyntacticallyCorrect(string dance)
+{
+    const string directions = "LlDdUuRr";
+
+    // loops through every beat
+    while (dance.find("/") != string::npos)
+    {
+        // finds contents in beat
         int index = dance.find("/");
         string prefix = dance.substr(0, index);
         dance = dance.substr(dance.find("/") + 1);
 
-        //checks if the contents are syntactically correct
-        if (prefix.length() == 0) continue;
-        else if (prefix.length() == 1 && directions.find(prefix) != string::npos) continue;
-        else if (prefix.length() == 2 && isdigit(prefix.at(0)) && directions.find(prefix.at(1)) != string::npos) continue;
-        else if (prefix.length() == 3 && isdigit(prefix.at(0)) && isdigit(prefix.at(1)) && directions.find(prefix.at(2)) != string::npos) continue;
-        else return false;
+        // checks if the contents are syntactically correct
+        if (prefix.length() == 0)
+            continue;
+        else if (prefix.length() == 1 && directions.find(prefix) != string::npos)
+            continue;
+        else if (prefix.length() == 2 && isdigit(prefix.at(0)) && directions.find(prefix.at(1)) != string::npos)
+            continue;
+        else if (prefix.length() == 3 && isdigit(prefix.at(0)) && isdigit(prefix.at(1)) && directions.find(prefix.at(2)) != string::npos)
+            continue;
+        else
+            return false;
     }
 
-    //once all beats (dashes) are gone, checks theres nothing left
-    if (dance.length() != 0) return false;
+    // once all beats (dashes) are gone, checks theres nothing left
+    if (dance.length() != 0)
+        return false;
 
     return true;
 }
 
-int translateDance(string dance, string& instructions, int& badBeat) {
-    //checks if syntactically correct
-    if (!isSyntacticallyCorrect(dance)) return 1;
+int translateDance(string dance, string &instructions, int &badBeat)
+{
+    // checks if syntactically correct
+    if (!isSyntacticallyCorrect(dance))
+        return 1;
 
     int beatCount = 0;
     int freezeCount = 0;
     string output = "";
     char freezeValue = ' ';
 
-    while (dance.find("/") != string::npos) {
-        //finds contents in beat
+    // loops through every beat
+    while (dance.find("/") != string::npos)
+    {
+        // finds contents in beat
         int index = dance.find("/");
         string prefix = dance.substr(0, index);
         dance = dance.substr(dance.find("/") + 1);
-        //updates counts
-        beatCount ++;
+        // updates counts
+        beatCount++;
 
-        //freeze too short
-        if (prefix.length() == 2 && (prefix.at(0) == '0' || prefix.at(0) == '1')) {
+        // freeze too short (0, 00, 1, 01)
+        if (prefix.length() == 2 && (prefix.at(0) == '0' || prefix.at(0) == '1'))
+        {
             badBeat = beatCount;
             return 2;
         }
-        else if (prefix.length() == 3 && (prefix.substr(0, 2) == "00" || prefix.substr(0, 2) == "01")) {
+        else if (prefix.length() == 3 && (prefix.substr(0, 2) == "00" || prefix.substr(0, 2) == "01"))
+        {
             badBeat = beatCount;
             return 2;
         }
-        //freeze in effect: not enough beats
-        else if (freezeCount > 0 && prefix.length() != 0) {
+        // freeze in effect: not enough beats
+        else if (freezeCount > 0 && prefix.length() != 0)
+        {
             badBeat = beatCount;
             return 3;
         }
-        //freeze initialized: set freezeCount
-        else if (prefix.length() == 2) {
+        // freeze initialized: set freezeCount
+        else if (prefix.length() == 2)
+        {
             freezeCount = stoi(prefix.substr(0, 1));
             freezeValue = prefix.at(1);
         }
-        else if (prefix.length() == 3) {
+        else if (prefix.length() == 3)
+        {
             freezeCount = stoi(prefix.substr(0, 2));
             freezeValue = prefix.at(2);
         }
 
-        //update output
-        if (prefix.length() == 1) {
+        // update output
+        if (prefix.length() == 1)
+        {
             output += tolower(prefix.at(0));
         }
-        else if (freezeCount > 0) {
+        else if (freezeCount > 0)
+        {
             output += toupper(freezeValue);
         }
-        else if (prefix.length() == 0) {
+        else if (prefix.length() == 0)
+        {
             output += ".";
         }
-        else cout << "error.\n";
 
-        if (freezeCount > 0) freezeCount --;
+        // update freezeCount
+        if (freezeCount > 0)
+            freezeCount--;
     }
-    
-    //dance done: freeze not done
-    if (freezeCount > 0) {
+
+    // dance done: freeze not done
+    if (freezeCount > 0)
+    {
         badBeat = beatCount + 1;
         return 4;
     }
@@ -95,22 +119,30 @@ int translateDance(string dance, string& instructions, int& badBeat) {
     return 0;
 }
 
-int main() {
-    while (true) {
+int main()
+{
+    while (true)
+    {
+        // takes in hypothetical dance
         string input = "";
         cout << "Enter input (e to exit): ";
         getline(cin, input);
-        if (input == "e") break;
-        
+        if (input == "e")
+            break;
+
+        // analyzes dance
         string instructions = "";
         int badBeat = 0;
         int translation = translateDance(input, instructions, badBeat);
-        if (translation == 1) {
+        if (translation == 1)
+        {
             cout << "false" << endl;
         }
-        else if (translation == 0) {
+        else if (translation == 0)
+        {
             cout << "Translation: " << instructions << endl;
         }
-        else cout << "Error: " << translation << " BadBeat: " << badBeat << endl;
+        else
+            cout << "Error: " << translation << " BadBeat: " << badBeat << endl;
     }
 }
---
+++ Project4/array.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+//1
+int repeat(string a[], int n) {
+    if (n < 0) return -1;
+    for (int i = 0; i < n; i++) {
+        a[i] = a[i] + a[i];
+    }
+    return n;
+}
+
+//2
+int detectMatch(const string a[], int n, string target) {
+    if (n < 0) return -1;
+    for (int i = 0; i < n; i++) {
+        if (a[i] == target) return i;
+    }
+    return -1;
+}
+
+//3
+int detectMax(const string a[], int n) {
+    if (n <= 0) return -1;
+    int max = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] > a[max]) max=i;
+    }
+    return max;
+}
+
+//4
+int circleLeft(string a[], int n, int pos) {
+    if (n < 0 || pos < 0) return -1;
+    string eliminated = a[pos];
+    for (int i = pos; i < n - 1; i++) {
+        a[i] = a[i+1];
+    }
+    a[n-1] = eliminated;
+    return pos;
+}
+
+//5
+int enumerateRuns(const string a[], int n) {
+    if (n <= 0) return -1;
+    if (n == 0) return 0;
+
+    int count = 1;
+    for (int i = 1; i < n; i++) {
+        if (a[i-1] != a[i]) count++;
+    }
+    return count;
+}
+
+//6
+int flip(string a[], int n) {
+    if (n < 0) return -1;
+    for (int i = 0; i < n/2; i++) {
+        string temp = a[i];
+        a[i] = a[n-1-i];
+        a[n-1-i] = temp;
+    }
+    return n;
+}
+
+//7
+int detectDifference(const string a1[], int n1, const string a2[], int n2) {
+    if (n1 < 0 || n2 < 0) return -1;
+    int min = 0;
+    if (n1 < n2) min = n1;
+    else min = n2;
+
+    for (int i = 0; i < min; i++) {
+        if (a1[i] != a2[i]) return i;
+    }
+    return min;
+}
+
+//8
+int subsequence(const string a1[], int n1, const string a2[], int n2) {
+    if (n1 < 0 || n2 < 0) return -1;
+    if (n2 == 0) return 0;
+
+    for (int i = 0; i <= n1 - n2; i++) {
+        int output = i;
+        for (int j = 0; j < n2; j++) {
+            if (a1[i+j] != a2[j]) {
+                output = -1;
+            }
+        }
+        if (output != -1) return output;
+    }
+    return -1;
+}
+
+//9
+int detectAny(const string a1[], int n1, const string a2[], int n2) {
+    if (n1 < 0 || n2 < 0) return -1;
+
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {
+            if (a1[i] == a2[j]) return i;
+        }
+    }
+    return -1;
+}
+
+//10
+int split(string a[], int n, string splitter) {
+    if (n < 0) return -1;
+
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] < splitter) {
+            string temp = a[index];
+            a[index] = a[i];
+            a[i] = temp;
+            index ++;
+        }
+    }
+    
+    return index;
+}
+
+int main() {
+    //test variables
+    int test_length = 0;
+    string test[10000];
+    int test2_length = 0;
+    string test2[10000];
+    string parameter = "";
+    int test_num = 1;
+    int returnValue = 0;
+
+    //loop to allow multiple tests
+    while (true) {
+        //test parameters
+        cout << "Enter test number: ";
+        cin >> test_num;
+        cout << "Enter array length: ";
+        cin >> test_length;
+        cout << "Enter array (ctrl+c to exit):\n";
+        for (int i = 0; i < test_length; i++) {
+            cin >> test[i];
+        }
+
+        //extra parameters for specific tests
+        if (test_num == 2 || test_num == 4 || test_num == 10) {
+            cout << "Enter parameter: ";
+            cin >> parameter;
+        }
+        if (test_num == 7 || test_num == 8 || test_num == 9) {
+            cout << "Enter 2nd array length: ";
+            cin >> test2_length;
+            cout << "Enter 2nd array (ctrl+c to exit):\n";
+            for (int i = 0; i < test2_length; i++) {
+                cin >> test2[i];
+            }
+        }
+
+        //runs specific function
+        switch (test_num) {
+            case (1):
+                returnValue = repeat(test, test_length);
+                break;
+            case 2:
+                returnValue = detectMatch(test, test_length, parameter);
+                break;
+            case (3):
+                returnValue = detectMax(test, test_length);
+                break;
+            case (4):
+                returnValue = circleLeft(test, test_length, stoi(parameter));
+                break;
+            case (5):
+                returnValue = enumerateRuns(test, test_length);
+                break;
+            case (6):
+                returnValue = flip(test, test_length);
+                break;
+            case (7):
+                returnValue = detectDifference(test, test_length, test2, test2_length);
+                break;
+            case (8):
+                returnValue = subsequence(test, test_length, test2, test2_length);
+                break;
+            case (9):
+                returnValue = detectAny(test, test_length, test2, test2_length);
+                break;
+            case (10):
+                returnValue = split(test, test_length, parameter);
+                break;
+            default:
+                break;            
+        }
+
+        //prints return value and test array
+        cout << "Return Value: " << returnValue << endl;
+        for (int i = 0; i < test_length; i++) {
+            cout << test[i] << " ";
+        }
+        cout << endl;
+    }
+
+}
